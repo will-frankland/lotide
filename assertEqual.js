93770723a228cb178b2e8009851ec66d4e71dbec
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-03-07 17:50:51 -0500

    Initial commit

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+}
+console.log(assertEqual);
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Liverpool", "Liverpool");
+assertEqual("Beer", 8);
+assertEqual(27, 3);
