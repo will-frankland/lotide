0800e2c1db766bcfea74dc80a80de9c2e42c12db
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-03-09 17:08:42 -0500

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+// const assertEqual = function(actual, expected) {
+//   if(actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// }
+
+const countLetters = function (string) {
+  // create an object to store the letters
+  let results = {};
+  // loop over the letters in the string
+  for (let letter of string) {
+    // output results
+    // results += string[letter];
+    // if letter has not been counted create it
+    if (!results[letter]) {
+      results[letter] = 1;
+    } else {
+      results[letter]++;
+    }
+  }
+  return results;
+}
+
+console.log(countLetters("sentence"))
