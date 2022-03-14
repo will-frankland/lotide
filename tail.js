e1aa651dd8364e363624fb4ba177dc2e379fed65
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-03-14 13:44:52 -0400

    refactoring with require and export

--- tail.js
+++ tail.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 console.log(assertEqual);
 
 const tail = function(arr1) {
@@ -15,6 +10,4 @@ const tail = function(arr1) {
   }
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
